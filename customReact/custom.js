e2d18db326257ff245d5a8df59b6e2777bf0aa6d
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-27 22:21:56 +0530

    #

--- customReact/custom.js
+++ customReact/custom.js
@@ -1,3 +1,11 @@
+function customRender(reactElement,mainContainer){
+    const domElement = document.createElement
+    (reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href',reactElement.props.target)
+    mainContainer.appendChild(domElement)
+}
+
 const reactElement ={
     type: 'a', 
     props: {
@@ -7,3 +15,4 @@ const reactElement ={
     children: 'click me to visit google'
 }
 const maincontainer = document.querySelector('#root')
+CustomRender(reactElement, mainContainer)
