2e7cf84d8cdb0c402be700fce478bcb202282eba
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-10-01 23:31:49 +0530

    create reaact

--- vite-React/src/New.jsx
+++ vite-React/src/New.jsx
@@ -1,7 +1,44 @@
-function NewComponent() {
-    return <div>
-        <h1>New Component</h1>
-    </div>;
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App.jsx'
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
 }
 
-export default NewComponent;
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank' },
+    'click me to visit google',
+    anotherElement
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    reactElement
+  
+)
+
+
